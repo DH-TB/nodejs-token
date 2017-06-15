655069e2d61012bdadfadae5d4b74a70913f6e19
DH-TB <929659475@qq.com>
2017-06-15 19:49:20 +0800

    acheive token

--- app.js
+++ app.js
@@ -1,15 +1,73 @@
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
+app.use('/register', express.static('./public/register.html'));
+app.use('/login', express.static('./public/login.html'));
+
 const bodyParser = require('body-parser');
-const User  = require('./user');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 const jwt = require('jsonwebtoken');
+const config = require('./config');
+app.set('superSecret', config.secret);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-var port = process.env.PORT || 8080;
+const User = require('./user');
+app.post('/register', function (req, res, next) {
+    new User(req.body).save(function (err) {
+        if (err) return next(err);
+        console.log('User saved successfully');
+        res.json({success: true});
+    });
+});
+
+app.post('/login', function (req, res, next) {
+    User.findOne({name: req.body.name}, function (err, user) {
+        if (err) return next(err);
+        if (!user) {
+            res.json({success: false, message: 'Authentication failed. User not found.'});
+        }
+        else if (user) {
+            if (user.password != req.body.password) {
+                res.json({success: false, message: 'Authentication failed. Wrong password.'});
+            } else {
+                var token = jwt.sign(user, app.get('superSecret'), {});
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
+app.get('/getName',function (req, res, next) {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: 'Failed to authenticate token.'});
+            }
+            else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                res.send(decoded._doc);
+            }
+        });
+    }
+    else {// if there is no token
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+});
+
+var port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
+console.log('Listening on  http://localhost:' + port);
