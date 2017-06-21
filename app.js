c1979c13df09cc24dc12fe853e790e46a58c9f55
DH-TB <929659475@qq.com>
2017-06-21 14:35:10 +0800

    test deploy

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
+// mongoose.connect('mongodb://localhost/test');
 const jwt = require('jsonwebtoken');
 const config = require('./config');
 app.set('superSecret', config.secret);
+const session = require('express-session');
+app.use(session({
+    secret: config.secret,
+    cookie: {maxAge: 100000},//30 days
+    url: config.url
+}));
 
 const User = require('./user');
+
 app.post('/register', function (req, res, next) {
     new User(req.body).save(function (err) {
         if (err) return next(err);
@@ -24,50 +31,54 @@ app.post('/register', function (req, res, next) {
     });
 });
 
-app.post('/login', function (req, res, next) {
-    User.findOne({name: req.body.name}, function (err, user) {
-        if (err) return next(err);
-        if (!user) {
-            res.json({success: false, message: 'Authentication failed. User not found.'});
-        }
-        else if (user) {
-            if (user.password != req.body.password) {
-                res.json({success: false, message: 'Authentication failed. Wrong password.'});
-            } else {
-                var token = jwt.sign(user, app.get('superSecret'), {});
-                res.json({
-                    success: true,
-                    message: 'Enjoy your token!',
-                    token: token
-                });
-            }
-        }
-    });
+mongoose.connect(config.url);
+
+/*
+ app.post('/login', function (req, res, next) {
+ User.findOne({name: req.body.name}, function (err, user) {
+ if (err) return next(err);
+ if (!user) {
+ res.json({success: false, message: 'Authentication failed. User not found.'});
+ }
+ else if (user) {
+ if (user.password != req.body.password) {
+ res.json({success: false, message: 'Authentication failed. Wrong password.'});
+ } else {
+ var token = jwt.sign(user, app.get('superSecret'), {});
+ res.json({
+ success: true,
+ message: 'Enjoy your token!',
+ token: token
+ });
+ }
+ }
+ });
+ });
+
+ app.get('/getName',function (req, res, next) {
+ const token = req.body.token || req.query.token || req.headers['x-access-token'];
+ if (token) {
+ jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+ if (err) {
+ return res.json({success: false, message: 'Failed to authenticate token.'});
+ }
+ else {
+ // if everything is good, save to request for use in other routes
+ req.decoded = decoded;
+ res.send(decoded._doc);
+ }
+ });
+ }
+ else {// if there is no token
+ return res.status(403).send({
+ success: false,
+ message: 'No token provided.'
+ });
+ }
+ });
+ */
+
+const port = process.env.PORT || config.port;
+app.listen(port,()=>{
+    console.log('Listening on  http://localhost:' + port);
 });
-
-app.get('/getName',function (req, res, next) {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (token) {
-        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
-            if (err) {
-                return res.json({success: false, message: 'Failed to authenticate token.'});
-            }
-            else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                res.send(decoded._doc);
-            }
-        });
-    }
-    else {// if there is no token
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
-});
-
-var port = process.env.PORT || 3000;
-
-app.listen(port);
-console.log('Listening on  http://localhost:' + port);
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'secret': 'ilovescotchyscotch'
+    secret: 'ilovescotchyscotch',
+    url: 'mongodb://huanglizhen:dhtb472561212@ds133582.mlab.com:33582/ttms',
+    port:3000
 };
