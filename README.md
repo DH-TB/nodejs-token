6f250d945bc0a4bfb70c934f9be3258dbca73188
DH-TB <929659475@qq.com>
2017-08-15 12:03:13 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# nodejs-tooken-demo
+```
+npm start
+http://localhost:3000/register
+```
+
+可以通过控制台查看token信息
--- app.js
+++ app.js
@@ -1,84 +1,81 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+const User = require('./user');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/register', express.static('./public/register.html'));
 app.use('/login', express.static('./public/login.html'));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://localhost/test');
-const jwt = require('jsonwebtoken');
-const config = require('./config');
 app.set('superSecret', config.secret);
-const session = require('express-session');
-app.use(session({
-    secret: config.secret,
-    cookie: {maxAge: 100000},//30 days
-    url: config.url
-}));
 
-const User = require('./user');
+mongoose.connect(config.url);
 
 app.post('/register', function (req, res, next) {
     new User(req.body).save(function (err) {
-        if (err) return next(err);
-        console.log('User saved successfully');
+        if (err) {
+            return next(err);
+        }
+        console.log('用户注册成功');
         res.json({success: true});
     });
 });
 
-mongoose.connect(config.url);
+app.post('/login', function (req, res, next) {
+    User.findOne({name: req.body.name}, function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            res.json({success: false, message: '没有找到该用户'});
+        }
+        else if (user) {
+            if (user.password != req.body.password) {
+                res.json({success: false, message: '密码错误'});
+            }
+            else {
+                let token = jwt.sign(user, app.get('superSecret'), {});
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
 
+app.get('/getName', function (req, res, next) {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
- app.post('/login', function (req, res, next) {
- User.findOne({name: req.body.name}, function (err, user) {
- if (err) return next(err);
- if (!user) {
- res.json({success: false, message: 'Authentication failed. User not found.'});
- }
- else if (user) {
- if (user.password != req.body.password) {
- res.json({success: false, message: 'Authentication failed. Wrong password.'});
- } else {
- var token = jwt.sign(user, app.get('superSecret'), {});
- res.json({
- success: true,
- message: 'Enjoy your token!',
- token: token
- });
- }
- }
- });
- });
-
- app.get('/getName',function (req, res, next) {
- const token = req.body.token || req.query.token || req.headers['x-access-token'];
- if (token) {
- jwt.verify(token, app.get('superSecret'), function (err, decoded) {
- if (err) {
- return res.json({success: false, message: 'Failed to authenticate token.'});
- }
- else {
- // if everything is good, save to request for use in other routes
- req.decoded = decoded;
- res.send(decoded._doc);
- }
- });
- }
- else {// if there is no token
- return res.status(403).send({
- success: false,
- message: 'No token provided.'
- });
- }
- });
+    if (token) {
+        /*验证token*/
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: '验证token失败'});
+            }
+            else {
+                req.decoded = decoded;
+                res.send(decoded._doc);
+            }
+        });
+    }
+    else {
+        return res.status(403).send({
+            success: false,
+            message: '没有token'
+        });
+    }
+});
 
 
 const port = process.env.PORT || config.port;
-app.listen(port,()=>{
+app.listen(port, ()=> {
     console.log('Listening on  http://localhost:' + port);
 });
